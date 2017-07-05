ecf7a04c043f2fcfe1b25c9d26532f1077607613
xcode <nhatduy918@gmail.com>
2017-07-05 15:01:34 +0700

    Initial Commit

---
+++ Cau5_LeTranNhatDuy_13110024Tests/Cau5_LeTranNhatDuy_13110024Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Cau5_LeTranNhatDuy_13110024Tests.swift
+//  Cau5_LeTranNhatDuy_13110024Tests
+//
+//  Created by Cntt02 on 7/5/17.
+//  Copyright © 2017 HCMUTE. All rights reserved.
+//
+
+import XCTest
+@testable import Cau5_LeTranNhatDuy_13110024
+
+class Cau5_LeTranNhatDuy_13110024Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
